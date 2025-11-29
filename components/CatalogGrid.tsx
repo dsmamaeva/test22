172d07c5d3b55acbb5fc2b94eec5dbaabb8f8b1e
dsmamaeva <dsmamaeva0412@gmail.com>
2025-11-29 18:12:05 +0300

    feat: Make catalog items linkable
    Allows individual catalog items to be wrapped in anchor tags, enabling navigation to specific product pages or sections. Updates the `CatalogItem` component to conditionally render as an `<a>` tag if an `href` prop is provided. The "Industrial Ovens" item in `index.html` is updated to use this new functionality.

--- components/CatalogGrid.tsx
+++ components/CatalogGrid.tsx
@@ -5,34 +5,42 @@ interface CatalogItemProps {
   className?: string;
   image: string;
   textPosition?: 'left' | 'center';
+  href?: string;
 }
 
-const CatalogItem: React.FC<CatalogItemProps> = ({ title, className = "", image, textPosition = 'left' }) => (
-  <div className={`relative group overflow-hidden bg-gray-900 ${className} h-[264px]`}>
-    {/* Image */}
-    <img 
-      src={image} 
-      alt={title}
-      className="absolute inset-0 w-full h-full object-cover transition-transform duration-700 group-hover:scale-105 opacity-80"
-    />
-    
-    {/* Overlay */}
-    <div className="absolute inset-0 bg-gradient-to-t from-black/70 via-black/20 to-black/40 group-hover:from-black/80 group-hover:to-black/50 transition-colors duration-300" />
-    
-    {/* Text Content */}
-    <div className={`absolute inset-0 p-6 md:p-8 flex flex-col ${
-      textPosition === 'center' 
-        ? 'items-center text-center pt-8 md:pt-10' 
-        : 'justify-center items-start text-left pl-8 md:pl-12'
-    }`}>
-      <h3 className="font-display text-2xl md:text-3xl lg:text-4xl leading-[0.9] text-white uppercase tracking-wider drop-shadow-lg">
-        {title.split('\n').map((line, i) => (
-          <span key={i} className="block mb-1">{line}</span>
-        ))}
-      </h3>
-    </div>
-  </div>
-);
+const CatalogItem: React.FC<CatalogItemProps> = ({ title, className = "", image, textPosition = 'left', href }) => {
+  const Component = href ? 'a' : 'div';
+  
+  return (
+    <Component 
+      href={href}
+      className={`relative group overflow-hidden bg-gray-900 ${className} h-[264px] block`}
+    >
+      {/* Image */}
+      <img 
+        src={image} 
+        alt={title}
+        className="absolute inset-0 w-full h-full object-cover transition-transform duration-700 group-hover:scale-105 opacity-80"
+      />
+      
+      {/* Overlay */}
+      <div className="absolute inset-0 bg-gradient-to-t from-black/70 via-black/20 to-black/40 group-hover:from-black/80 group-hover:to-black/50 transition-colors duration-300" />
+      
+      {/* Text Content */}
+      <div className={`absolute inset-0 p-6 md:p-8 flex flex-col ${
+        textPosition === 'center' 
+          ? 'items-center text-center pt-8 md:pt-10' 
+          : 'justify-center items-start text-left pl-8 md:pl-12'
+      }`}>
+        <h3 className="font-display text-2xl md:text-3xl lg:text-4xl leading-[0.9] text-white uppercase tracking-wider drop-shadow-lg">
+          {title.split('\n').map((line, i) => (
+            <span key={i} className="block mb-1">{line}</span>
+          ))}
+        </h3>
+      </div>
+    </Component>
+  );
+};
 
 const CatalogGrid: React.FC = () => {
   return (
@@ -54,6 +62,7 @@ const CatalogGrid: React.FC = () => {
           className="lg:col-span-3"
           image="https://codia-f2c.s3.us-west-1.amazonaws.com/image/2025-11-27/iNtpTKAmN6.png"
           textPosition="left"
+          href="lab.html"
         />
         <CatalogItem 
           title={'Сервисное\nобслуживание'}
@@ -66,8 +75,9 @@ const CatalogGrid: React.FC = () => {
         <CatalogItem 
           title="Промышленные печи" 
           className="lg:col-span-2"
-          image="https://codia-f2c.s3.us-west-1.amazonaws.com/image/2025-11-27/fFKOob8vHA.png"
+          image="Img/imgprom.png"
           textPosition="center"
+          href="prom.html"
         />
         <CatalogItem 
           title="Туннельные печи" 
